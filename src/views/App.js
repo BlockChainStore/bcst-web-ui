e3c46fc6a8bc9cbb847c6fa38c086301b005b046
sittisak <sittisak.churitayo@gmail.com>
2018-07-19 12:55:26 +0700

    setup react ruuter

--- src/views/App.js
+++ src/views/App.js
@@ -1,16 +1,34 @@
 import './assets/main.css'
 import React from 'react'
+import { Provider } from 'react-redux'
 import { MuiThemeProvider } from '@material-ui/core/styles'
+import { BrowserRouter, Switch, Route  } from 'react-router-dom'
 import webTheme from './theme'
 import HeadWrapper from './head-wrapper'
+import store from '../store'
+
+
+const Home = () => <p>home</p>
+const Dashboard = () => <p>dashboard</p>
+const Investment = () => <p>investment</p>
+
+
 
 const App = () => {
 	return (
-		<MuiThemeProvider theme={webTheme}>
-			<HeadWrapper>
-				<p>sittisak</p>
-			</HeadWrapper>
-		</MuiThemeProvider>
+		<Provider store={store}>
+			<MuiThemeProvider theme={webTheme}>
+				<BrowserRouter>
+					<HeadWrapper>
+						<Switch>
+							<Route exact path='/' component={Home} />
+							<Route exact path='/dashboard' component={Dashboard} />
+							<Route exact path='/investment' component={Investment} />
+						</Switch>
+					</HeadWrapper>
+				</BrowserRouter>
+			</MuiThemeProvider>
+		</Provider>
 	)
 }
 
--- src/views/head-wrapper/menu-list.js
+++ src/views/head-wrapper/menu-list.js
@@ -1,35 +1,47 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import { withStyles } from '@material-ui/core/styles'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
 
+import Home from '@material-ui/icons/Home'
 import Dashboard from '@material-ui/icons/Dashboard'
 import Money from '@material-ui/icons/AssignmentReturned'
 
-const styles = theme => ({})
 
-const MenuList = () => {
+const styles = theme => ({
+    link: {
+        textDecoration: 'none'
+    }
+})
+
+const MenuList = ({classes}) => {
     return (
         <List component="nav">
-            <ListItem button>
-                <ListItemIcon><Dashboard /></ListItemIcon>
-                <ListItemText primary="Dashboard" />
-            </ListItem>
-            <ListItem button>
-                <ListItemIcon><Money /></ListItemIcon>
-                <ListItemText primary="Investment" />
-            </ListItem>
+            <Link to='/' className={classes.link}>
+                <ListItem button>
+                    <ListItemIcon><Home /></ListItemIcon>
+                    <ListItemText primary="Home" />
+                </ListItem>
+            </Link>
+            <Link to='/dashboard' className={classes.link}>
+                <ListItem button>
+                    <ListItemIcon><Dashboard /></ListItemIcon>
+                    <ListItemText primary="Dashboard" />
+                </ListItem>
+            </Link>
+            <Link to='/investment' className={classes.link}>
+                <ListItem button>
+                    <ListItemIcon><Money /></ListItemIcon>
+                    <ListItemText primary="Investment" />
+                </ListItem>
+            </Link>
         </List>
     )
 }
-
-MenuList.propTypes = {
-    classes: PropTypes.object.isRequired,
-    theme: PropTypes.object.isRequired,
-} 
 
 export default withStyles(styles)(MenuList)
 
