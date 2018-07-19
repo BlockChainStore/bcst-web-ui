9b7cdef4fc7daf336ea62ef548601e72c53966ee
kanin <kaninhub@hotmail.com>
2018-07-19 16:54:32 +0700

    add table

--- src/views/App.js
+++ src/views/App.js
@@ -7,13 +7,11 @@ import { ConnectedRouter } from "react-router-redux"
 import webTheme from './theme'
 import HeadWrapper from './head-wrapper'
 import store, { history } from '../store'
-
+import  WithStyles  from '../views/pages/investment.js'
 
 const Home = () => <p>home</p>
 const Dashboard = () => <p>dashboard</p>
-const Investment = () => <p>investment</p>
-
-
+const Investment = () => <WithStyles />
 
 const App = () => {
 	return (
