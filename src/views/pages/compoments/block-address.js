98e7d795a505b021988ad1b935f27bc613db3ad3
sittisak <sittisak.churitayo@gmail.com>
2018-07-31 16:12:08 +0700

    display address, ETH amount and BCST amount

---
+++ src/views/pages/compoments/block-address.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+
+
+const styles = theme => ({
+	paper: {
+		...theme.mixins.gutters(),
+		paddingTop: theme.spacing.unit * 2,
+		paddingBottom: theme.spacing.unit * 2
+	}
+})
+
+const blockCurrency = (props) => {
+	const { classes, user } = props
+	return (
+		<Paper className={classes.paper} elevation={1}>
+			<Typography variant="headline" gutterBottom>
+				Your Address
+			</Typography>
+			<Typography color="textSecondary" gutterBottom >
+				{user.info.address}
+			</Typography>
+		</Paper>
+	)
+}
+
+
+const mapStateToProps = state => ({
+    user: state.duck.user
+})
+
+export default connect(
+    mapStateToProps,
+    null
+)(withStyles(styles)(blockCurrency))
