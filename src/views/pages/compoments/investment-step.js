c3e2a17c89f483371dc7bde614f6c72a2847e469
kanin <kaninhub@hotmail.com>
2018-08-24 17:46:37 +0700

    fix

--- src/views/pages/compoments/investment-step.js
+++ src/views/pages/compoments/investment-step.js
@@ -190,10 +190,11 @@ class InvestmentStep extends React.Component {
     }
 
     handleNext = () => {
-        const maxVal = this.state.bcst > 300000 ? parseInt(this.state.bcst) : 300000
+   
         switch (this.state.activeStep) {
             case 1:
-                const validator = Joi.number().min(10001).max(maxVal)
+                const validator = Joi.number().min(10001).max(300000)
+                debugger
                 const result = Joi.validate(this.state.bcst, validator)
 
                 if(!!result.error) {
